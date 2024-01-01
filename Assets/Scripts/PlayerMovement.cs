337e88b341771449fb8f499ef2ea1738c61fcaba
Sagnik Dasgupta <sagnik2000trainstation@gmail.com>
2024-01-01 21:44:22 +0530

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,37 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 playerInput;
+    Rigidbody2D rb;
+    [SerializeField] private float speed = 5.0f;
     void Start()
     {
-        
+     rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Flip();
         
     }
     void OnMove(InputValue value)
     {
         playerInput = value.Get<Vector2>();
-        Debug.Log(playerInput);
+        //Debug.Log(playerInput);
+    }
+    void Move()
+    {
+        Vector2 playerMove = new Vector2(playerInput.x*speed, rb.velocity.y);
+        rb.velocity = playerMove;
+    }
+    void Flip()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (hasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
     }
     
 }
