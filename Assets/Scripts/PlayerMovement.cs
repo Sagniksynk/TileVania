605aad2236ef51784c4989a201e2f12175323da0
Sagnik Dasgupta <sagnik2000trainstation@gmail.com>
2024-01-02 01:09:05 +0530

    Added running animation to code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,12 @@ public class PlayerMovement : MonoBehaviour
     Vector2 playerInput;
     Rigidbody2D rb;
     [SerializeField] private float speed = 5.0f;
+    Animator animator;
+    private string running_anim = "isRunning";
     void Start()
     {
-     rb = GetComponent<Rigidbody2D>();   
+     rb = GetComponent<Rigidbody2D>(); 
+     animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,17 +28,19 @@ public class PlayerMovement : MonoBehaviour
         playerInput = value.Get<Vector2>();
         //Debug.Log(playerInput);
     }
-    void Move()
+    public void Move()
     {
         Vector2 playerMove = new Vector2(playerInput.x*speed, rb.velocity.y);
         rb.velocity = playerMove;
-    }
-    void Flip()
-    {
         bool hasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        animator.SetBool(running_anim, hasHorizontalSpeed);
+    }
+    public void Flip()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon; 
         if (hasHorizontalSpeed)
         {
-            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+           transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f); //flipping
         }
     }
     
